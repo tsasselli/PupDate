b7f09769691a09c28781fd1401e6048a99750930
tsasselli <tsasselli@gmail.com>
2018-08-08 15:30:01 -0700

    added broad cors property, retrieving data in angular from db

--- PupDate-SPA/src/app/app.module.ts
+++ PupDate-SPA/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ValueComponent } from './value/value.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ValueComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
