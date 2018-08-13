f5a3507c64bb5f2994d5f710fe09edb9ff522f8c
tsasselli <tsasselli@gmail.com>
2018-08-13 16:14:38 -0700

    added bootstrap navbar component and setup

--- PupDate-SPA/src/app/app.module.ts
+++ PupDate-SPA/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ValueComponent } from './value/value.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ValueComponent
+    ValueComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
