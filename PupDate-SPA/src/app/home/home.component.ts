7ee8913abf48c90b32c4949a33ede8ed917c1467
tsasselli <tsasselli@gmail.com>
2018-08-13 22:55:31 -0700

    registered users to the db

--- PupDate-SPA/src/app/home/home.component.ts
+++ PupDate-SPA/src/app/home/home.component.ts
@@ -9,23 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   registerMode: boolean = false;
-  values: any;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.getValues();
   }
 
   registerToggle() {
-    this.registerMode = !this.registerMode;
+    this.registerMode = true;
   }
 
-  getValues() {
-    this.http.get('http://localhost:5000/api/values').subscribe(response => {
-      this.values = response;
-    }, error => {
-      console.log(error);
-    });
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
   }
 }
