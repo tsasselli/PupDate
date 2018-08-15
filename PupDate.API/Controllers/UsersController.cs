988ecbc4b24870135724845d9417eb3fae7d25fe
tsasselli <tsasselli@gmail.com>
2018-08-15 14:35:44 -0700

    added userController for api, fetched users from db/postman

---
+++ PupDate.API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PupDate.API.Data;
+
+namespace PupDate.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IDatingRepository _repo;
+
+        public UsersController(IDatingRepository repo)
+        {
+            _repo = repo;
+
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _repo.GetUsers();
+            
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _repo.GetUser(id);
+            
+            return Ok(user);
+        }
+    }
+}
