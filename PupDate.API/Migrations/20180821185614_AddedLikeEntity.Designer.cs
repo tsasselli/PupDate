4e7449faec1d59ef3c273ccde91d6277beef6c65
tsasselli <tsasselli@gmail.com>
2018-08-21 12:01:27 -0700

    added override for like migration, updated the db with likeeId and LikerId

--- PupDate.API/Migrations/20180821185614_AddedLikeEntity.Designer.cs
+++ PupDate.API/Migrations/20180821185614_AddedLikeEntity.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PupDate.API.Data;
 
 namespace PupDate.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20180821185614_AddedLikeEntity")]
+    partial class AddedLikeEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
